45de71fd4c55b8faaa0aa40db4a4b95da6285b6e
NothingAt-All <std.nullptr_t@outlook.com>
2023-09-16 15:22:45 -0500

    ajuste de eventos

--- src/eventos.cpp
+++ src/eventos.cpp
@@ -1,10 +1,10 @@
 #include "eventos.hpp"
+#include "identificadores.hpp"
 #include <windows.h>
-#include <iostream>
+
 
 UINT emisor(WPARAM interaccion) { return LOWORD(interaccion); }
 UINT accion(WPARAM interaccion) { return HIWORD(interaccion); }
-bool atrapa(UINT interaccion, DWORD tipo, UINT identificador) { return emisor(interaccion) == identificador && accion(interaccion) == tipo; }
 
 LRESULT CALLBACK eventos(HWND ventana, UINT evento, WPARAM interaccion, LPARAM datos_interaccion)
 {
@@ -14,7 +14,17 @@ LRESULT CALLBACK eventos(HWND ventana, UINT evento, WPARAM interaccion, LPARAM d
         case WM_DESTROY: PostQuitMessage(0); break;
         case WM_COMMAND: 
         {
-            
+            if(emisor(interaccion) == BOTON_1 && accion(interaccion) == BN_CLICKED)
+            {
+                HWND boton_2 = GetDlgItem(ventana, BOTON_2);
+                SetWindowText(boton_2, "Hola");
+            }
+            else 
+            if(emisor(interaccion) == BOTON_2 && accion(interaccion) == BN_CLICKED)
+            {
+                HWND boton_1 = GetDlgItem(ventana, BOTON_1);
+                SetWindowText(boton_1, "Hola");
+            }
         }
     }
 
